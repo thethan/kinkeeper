97c4e5db897e6b38276f72abf38e8625357cc7cb
ethan <ethan.totten@gmail.com>
2019-11-08 07:56:50 -0800

    adding the consumer groups register and tests

--- consumers/groups.go
+++ consumers/groups.go
@@ -18,6 +18,7 @@ type Consumer interface {
 // Store saves the iteration into the database in order to pull the next record from the next consumer
 type Store interface {
 	Store(ctx context.Context, )
+	PullConsumerGroup(ctx context.Context) (map[string]ConsumerData, error)
 }
 
 // namespace is where the shardID and stream names are located.
@@ -44,29 +45,78 @@ type ConsumerGroup struct {
 }
 
 // NewConsumerGroup creates a consumer group
-func NewConsumerGroup(log *log.Logger, store Store, streamName string, shardID string) ConsumerGroup {
+func NewConsumerGroup(ctx context.Context, log *log.Logger, store Store, streamName string, shardID string) ConsumerGroup {
 	cg := ConsumerGroup{
 		log: log,
 		namespace: namespace{
-			shardID: shardID, streamName: streamName,
+			shardID:    shardID,
+			streamName: streamName,
 		},
 		store: store,
 	}
 
 	cg.consumers = sync.Map{}
+	go cg.start(ctx)
 	return cg
 }
 
 // consumerData
 type ConsumerData struct {
-	partitionKey *string
-	lastRead     *time.Time
+	partitionKey   *string
+	lastRead       *time.Time
+	timeOfRegister time.Time
+}
+
+// start listening for other consumers
+func (cg *ConsumerGroup) start(ctx context.Context) error {
+
+	readErrorChan := make(chan error)
+	// pull from the data store
+
+	ticker := time.NewTicker(5 * time.Millisecond)
+
+	//// read on load
+	//cg.readAndLoadConsumers(ctx, readErrorChan)
+
+	for {
+		select {
+		case <-ctx.Done():
+			cg.log.Print("context closed ")
+			close(readErrorChan)
+			return nil
+		case err := <-readErrorChan:
+			cg.log.Print("error in error channel. Closing channel ", "err ", err)
+			close(readErrorChan)
+			return nil
+		case <-ticker.C:
+			cg.readAndLoadConsumers(ctx, readErrorChan)
+
+		}
+	}
+}
+
+func (cg *ConsumerGroup) readAndLoadConsumers(ctx context.Context, errorChan chan error) {
+	consumers, err := cg.store.PullConsumerGroup(ctx)
+	if err != nil {
+		errorChan <- err
+	}
+	err = loadConsumers(ctx, cg, consumers)
+	if err != nil {
+		errorChan <- err
+	}
+}
+
+func loadConsumers(ctx context.Context, consumerGroup *ConsumerGroup, consumersDataObject map[string]ConsumerData) error {
+	for key, data := range consumersDataObject {
+		consumerGroup.consumers.Store(key, data)
+	}
+	return nil
 }
 
 // Register a consumer.
 // A consumer will be added to the loop in order to read from the checkpoint store.
 func (cg *ConsumerGroup) Register(ctx context.Context, identifier string) (*ConsumerData, error) {
-	data, alreadyExists := cg.consumers.LoadOrStore(identifier, &ConsumerData{})
+	data, alreadyExists := cg.consumers.LoadOrStore(identifier, &ConsumerData{timeOfRegister:time.Now()})
 
 	cg.log.Printf("ConsumerData already existed if true: %v, else it was stored for identifier: %s\n", alreadyExists, identifier)
 
@@ -78,7 +128,7 @@ func (cg *ConsumerGroup) Register(ctx context.Context, identifier string) (*Cons
 	return cData, nil
 }
 
-// CheckIfConsumerIsNextInSequence
+// CheckIfConsumerIsNextInSequence if the
 func (cg *ConsumerGroup) CheckIfConsumerIsNextInSequence(ctx context.Context, identifier string) {
 
 }
--- consumers/groups_test.go
+++ consumers/groups_test.go
@@ -2,42 +2,82 @@ package consumers
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
 	"log/syslog"
 	"testing"
+	"time"
 )
 
-type MockStore struct {
-	
+type MockStoreInput struct {
+	PullContexts []context.Context
 }
 
-func (MockStore) Store(ctx context.Context, ) {
-	panic("implement me")
+type MockStoreOutput struct {
+	MapConsumerData         []map[string]ConsumerData
+	PullConsumerGroupErrors []error
+}
+
+type MockStore struct {
+	Output MockStoreOutput
+	Input  MockStoreInput
+}
+
+func MakeMockStore() MockStore {
+	return MockStore{
+		Input: MockStoreInput{
+			PullContexts: make([]context.Context, 0),
+		},
+		Output: MockStoreOutput{
+			MapConsumerData:         make([]map[string]ConsumerData, 0),
+			PullConsumerGroupErrors: make([]error, 0),
+		},
+	}
+}
+
+func (m *MockStore) Store(ctx context.Context) {
+	m.Input.PullContexts = append(m.Input.PullContexts, ctx)
+}
+
+func (m *MockStore) PullConsumerGroup(ctx context.Context) (map[string]ConsumerData, error) {
+	i := len(m.Input.PullContexts)
+	m.Input.PullContexts = append(m.Input.PullContexts, ctx)
+
+	return m.Output.MapConsumerData[i], m.Output.PullConsumerGroupErrors[i]
 }
 
 func TestConsumerGroup_Register(t *testing.T) {
 	logger, _ := syslog.NewLogger(syslog.LOG_INFO, 1)
 	shardID := "0000"
 	streamName := "streamName"
-	cg := NewConsumerGroup(logger, &MockStore{}, streamName, shardID)
+	mockStore := MakeMockStore()
+	mockStore.Output.MapConsumerData = []map[string]ConsumerData{map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}}}
+	mockStore.Output.PullConsumerGroupErrors = []error{nil}
+
+	cg := NewConsumerGroup(context.Background(), logger, &mockStore, streamName, shardID)
 	assert.IsType(t, ConsumerGroup{}, cg)
 
+	timeBeforeRegister := time.Now()
 	data, err := cg.Register(context.Background(), "consumer1")
 	assert.Nil(t, err)
 	assert.IsType(t, &ConsumerData{}, data)
+
+	assert.True(t, timeBeforeRegister.Before(data.timeOfRegister), "registered time should be after test starts now")
+	timeAfterRegister := time.Now()
+	assert.True(t, timeAfterRegister.After(data.timeOfRegister))
 
 	data2, err := cg.Register(context.Background(), "consumer1")
 	assert.Nil(t, err)
 	assert.IsType(t, &ConsumerData{}, data2)
 	assert.Equal(t, data, data2)
 }
-
 
 func TestConsumerGroup_Register_With_AlreadyRegistered(t *testing.T) {
 	logger, _ := syslog.NewLogger(syslog.LOG_INFO, 1)
 	shardID := "0000"
 	streamName := "streamName"
-	cg := NewConsumerGroup(logger,&MockStore{},  streamName, shardID)
+	mockStore := MakeMockStore()
+	cg := NewConsumerGroup(context.Background(), logger, &mockStore, streamName, shardID)
 	assert.IsType(t, ConsumerGroup{}, cg)
 
 	data, err := cg.Register(context.Background(), "consumer1")
@@ -50,3 +90,82 @@ func TestConsumerGroup_Register_With_AlreadyRegistered(t *testing.T) {
 	assert.Equal(t, data, data2)
 }
 
+func TestConsumerGroup_CallingPullFromAtLeastOnce(t *testing.T) {
+	logger, _ := syslog.NewLogger(syslog.LOG_INFO, 1)
+	shardID := "0000"
+	streamName := "streamName"
+	mockStore := MakeMockStore()
+	mockStore.Output.MapConsumerData = []map[string]ConsumerData{
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}}}
+	mockStore.Output.PullConsumerGroupErrors = []error{nil, nil, nil, nil, nil, nil, nil, nil}
+
+	ctx := context.Background()
+	cg := NewConsumerGroup(ctx, logger, &mockStore, streamName, shardID)
+	assert.IsType(t, ConsumerGroup{}, cg)
+
+	// Read from gets called every 5 milliseconds
+	time.Sleep(time.Millisecond)
+	assert.Greater(t, 1, len(mockStore.Input.PullContexts), "Pull from store was not called once")
+
+	ctx.Done()
+}
+
+func Test_readAndLoadConsumers(t *testing.T) {
+	logger, _ := syslog.NewLogger(syslog.LOG_INFO, 1)
+	shardID := "0000"
+	streamName := "streamName"
+	mockStore := MakeMockStore()
+	mockStore.Output.MapConsumerData = []map[string]ConsumerData{
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+	}
+	mockStore.Output.PullConsumerGroupErrors = []error{nil, nil, nil, nil, nil, nil, nil, nil}
+	ctx := context.Background()
+
+	cg := NewConsumerGroup(ctx, logger, &mockStore, streamName, shardID)
+
+	errChan := make(chan error)
+	cg.readAndLoadConsumers(ctx, errChan)
+
+	assert.Equal(t, 0, len(errChan))
+	lengthOfConsumers := 0
+	cg.consumers.Range(func(key, value interface{}) bool {
+		lengthOfConsumers++
+		return true
+	})
+	assert.Equal(t, 1, lengthOfConsumers, "did not have the right number of consumers")
+}
+
+
+
+func Test_readAndLoadConsumers_multiple(t *testing.T) {
+	logger, _ := syslog.NewLogger(syslog.LOG_INFO, 1)
+	shardID := "0000"
+	streamName := "streamName"
+	mockStore := MakeMockStore()
+	mockStore.Output.MapConsumerData = []map[string]ConsumerData{
+		map[string]ConsumerData{"ethan": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}, "not": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}, "not2": {partitionKey: aws.String("test"), lastRead: aws.Time(time.Now())}},
+		}
+	mockStore.Output.PullConsumerGroupErrors = []error{nil, nil, nil, nil, nil, nil, nil, nil}
+	ctx := context.Background()
+
+	cg := NewConsumerGroup(ctx, logger, &mockStore, streamName, shardID)
+
+	errChan := make(chan error)
+	cg.readAndLoadConsumers(ctx, errChan)
+
+	assert.Equal(t, 0, len(errChan))
+	lengthOfConsumers := 0
+	cg.consumers.Range(func(key, value interface{}) bool {
+		lengthOfConsumers++
+		return true
+	})
+	assert.Equal(t, 3, lengthOfConsumers, "did not have the right number of consumers")
+}
